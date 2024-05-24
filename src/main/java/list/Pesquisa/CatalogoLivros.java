03a358379477f5c61eb89783b90c142469328cdc
Luciano Barros <lucianof.barros89@gmail.com>
2024-05-24 18:31:15 -0300

    Enviando CataloLivros
    Signed-off-by: Luciano Barros <lucianof.barros89@gmail.com>

---
+++ src/main/java/list/Pesquisa/CatalogoLivros.java
@@ -0,0 +1,53 @@
+package main.java.list.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivros {
+    private List<Livro> livrosList;
+
+    public CatalogoLivros() {
+        this.livrosList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao) {
+        livrosList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor) {
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if (!livrosList.isEmpty()) {
+            for (Livro l : livrosList) {
+                if (l.getAutor().equalsIgnoreCase(autor)) {
+                    livrosPorAutor.add(l);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisarPorIntervaloAnos(int anoInicial, int anoFinal) {
+        List<Livro> livrosPorIntervalosAnos = new ArrayList<>();
+        if (!livrosList.isEmpty()) {
+            for (Livro l : livrosList) {
+                if (l.getAnoPublicacao() >= anoInicial && l.getAnoPublicacao() <= anoFinal) {
+                    livrosPorIntervalosAnos.add(l);
+                }
+            }
+        }
+        return livrosPorIntervalosAnos;
+    }
+
+    public Livro pesquisarPorTitulo(String titulo) {
+        Livro livroPorTitulo = null;
+        if (!livrosList.isEmpty()) {
+            for (Livro l : livrosList) {
+                if (l.getTitulo().equalsIgnoreCase(titulo)) {
+                    livroPorTitulo = l;
+                    break;
+                }
+            }
+        }
+        return livroPorTitulo;
+    }
+}
