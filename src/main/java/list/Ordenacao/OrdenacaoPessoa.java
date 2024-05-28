56b14db8f5cb4b9841f918cafe1df03e1d23ade9
Luciano Barros <lucianof.barros89@gmail.com>
2024-05-27 23:24:18 -0300

    Enviando main ordenacao

--- src/main/java/list/Ordenacao/OrdenacaoPessoa.java
+++ src/main/java/list/Ordenacao/OrdenacaoPessoa.java
@@ -26,4 +26,18 @@ public class OrdenacaoPessoa {
         Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
         return pessoasPorAltura;
     }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("nome 1", 25, 1.78);
+        ordenacaoPessoa.adicionarPessoa("nome 2", 27, 1.80);
+        ordenacaoPessoa.adicionarPessoa("nome 3", 30, 1.65);
+        ordenacaoPessoa.adicionarPessoa("nome 4", 18, 1.52);
+        ordenacaoPessoa.adicionarPessoa("nome 5", 35, 1.60);
+        ordenacaoPessoa.adicionarPessoa("nome 6", 20, 1.72);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+
 }
