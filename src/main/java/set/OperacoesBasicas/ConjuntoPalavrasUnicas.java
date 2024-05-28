a8568543c4b3c8f601b74af0f4f13c33666608d8
Luciano Barros <lucianof.barros89@gmail.com>
2024-05-28 13:39:17 -0300

    enviando exercícios ConjuntoPalavrasUnicas

---
+++ src/main/java/set/OperacoesBasicas/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,63 @@
+package main.java.set.OperacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoPalavrasUnicas {
+    private Set<String> palavrasUnicas;
+
+    public ConjuntoPalavrasUnicas() {
+        this.palavrasUnicas = new HashSet<>();
+    }
+
+    public void adicionarPalavra(String palavra) {
+        palavrasUnicas.add(palavra);
+    }
+
+    public void removerPalavra(String palavra) {
+        String palavraParaRemover = null;
+        for (String p : palavrasUnicas) {
+            if (palavrasUnicas.contains(p)) {
+                palavraParaRemover = palavra;
+                break;
+            } else {
+                throw new IllegalArgumentException("Esta palavra não existe");
+            }
+        }
+        palavrasUnicas.remove(palavraParaRemover);
+    }
+
+    public void verificarPalavra(String palavra) {
+        String palavraParaVerificar = palavra;
+        if (palavrasUnicas.contains(palavraParaVerificar)) {
+            System.out.println("A palavra " + palavraParaVerificar + " está no Set.");
+        } else {
+            System.out.println("Esta palavra não existe");
+        }
+    }
+
+    public void exibirPalavrasUnicas() {
+        if (!palavrasUnicas.isEmpty()) {
+            System.out.println(palavrasUnicas);
+        } else {
+            throw new IllegalArgumentException("O set está vazio");
+        }
+    }
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas palavras = new ConjuntoPalavrasUnicas();
+
+        palavras.adicionarPalavra("Azul");
+        palavras.adicionarPalavra("Verde");
+        palavras.adicionarPalavra("Amarelo");
+        palavras.adicionarPalavra("Roxo");
+
+        palavras.verificarPalavra("Azul");
+        palavras.verificarPalavra("Vermelho");
+
+        palavras.removerPalavra("Verde");
+
+        palavras.exibirPalavrasUnicas();
+    }
+}
+
